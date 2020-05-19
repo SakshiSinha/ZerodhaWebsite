9a7cccf934b23b79cb99b85cdf35d1cc2ccb34a2
SakshiSinha <sakshisinha202326@gmail.com>
2020-05-19 22:55:02 +0530

    Zerodha

---
+++ build/precache-manifest.4e060a9c89b8ecd763ceef3557d7247d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8faa116c0f31465406e42dd86b594e3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1a512b35ad4b79ed9e5",
+    "url": "/static/css/main.d8a7558f.chunk.css"
+  },
+  {
+    "revision": "cb6a725e974e188b0c76",
+    "url": "/static/js/2.87c06bce.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.87c06bce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f1a512b35ad4b79ed9e5",
+    "url": "/static/js/main.5f84d765.chunk.js"
+  },
+  {
+    "revision": "0480e8b9c0e7392d88ec",
+    "url": "/static/js/runtime-main.f9d35aa9.js"
+  }
+]);
